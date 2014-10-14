36e6b7cd1886de1c557c39ca0221c1a29c9ba23a
wisecrab <dowork@chrissteurer.com>
2014-10-14 16:04:05 -0400

    Basic Document Set Up
    The basic bootstrap files split into PHP template files.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+<?php include "/objects/head.php" ?>
+
+<body>
+  <?php include "/objects/nav.php" ?>
+
+  <div class="container">
+    <div class="starter-template">
+      <h1>Hello, world!</h1>
+        <p class="lead">Now you can start your own project with <a target="_blank" href="http://getbootstrap.com/">Bootstrap 3.2.0</a>. This plugin is a fork from <a href="https://github.com/le717/brackets-html-skeleton#readme">HTML Skeleton</a>.</p>
+    </div>
+  </div>
+
+  <?php include "/objects/footer.php" ?>
+</body>
+</html>
---
+++ objects/head.php
@@ -0,0 +1,16 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Starter Template for Bootstrap 3.2.0</title>
+  <link rel="shortcut icon" href="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+  <style>body{padding-top:50px;}.starter-template{padding:40px 15px;text-align:center;}</style>
+
+  <!--[if IE]>
+    <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
